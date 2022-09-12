64cfe0cdca1fc6b353be7f2da7d1b3461984fc2a
David Bello <davidbello998@gmail.com>
2022-09-12 04:29:58 -0700

    cnumbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,4 @@
-#inlcude <stdio.h>
+#include <stdio.h>
 
 /**
  * main - prints the alphabet in lowercase,
@@ -8,6 +8,7 @@
 int main(void)
 {
 	int n;
+
 	for (n = 0; n > 10; n++)
 		printf("%d", n);
 	printf("/n");
