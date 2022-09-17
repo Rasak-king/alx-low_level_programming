3a425a0043e82d6a6b7f448f1d55f549f13b8cb8
David Bello <davidbello998@gmail.com>
2022-09-16 17:40:49 -0700

    supper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,16 +8,10 @@
  */
 int _isupper(int c)
 {
-	char uppercase = 'A';
-	int isupper = 0;
-	
-	for (; uppercase <= 'Z'; uppercase++)
+	if (c >= 'A' && c <= 'Z')
 	{
-		if (c == uppercase)
-		{
-			isupper = 1;
-			break;
-		}
-	}
-	return (isupper);
+		return (1);
+	} else
+
+	return (0);
 }
